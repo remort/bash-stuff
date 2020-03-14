2123c20339baa29ef1d8c9484eafa1a0920212e3
Вадим Бажов <master@remort.net>
2020-03-14 22:05:05 +0300

    Более правильное использование scale у ffmpeg

--- tgmovie.sh
+++ tgmovie.sh
@@ -5,4 +5,4 @@ set -u -e
 FILE=$1
 FILENAME=${FILE%.*}
 
-ffmpeg -i ${FILE} -vf "scale=iw/2:ih/2" -c:v libx264 -preset slow -b:v 500k -c:a aac -b:a 128k "${FILENAME}.mp4"
+ffmpeg -i ${FILE} -vf "scale=iw/2:-2" -c:v libx264 -preset slow -b:v 500k -c:a aac -b:a 128k "${FILENAME}.mp4"
